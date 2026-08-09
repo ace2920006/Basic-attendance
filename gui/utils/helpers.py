6a773fcb4243d2756f34e24836cf0b50719476d6
ace2920006 <dilipbawane124@gmail.com>
2026-08-09 14:12:08 +0530

    22

--- gui/utils/helpers.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-def today_str():
-    return datetime.now().strftime("%Y-%m-%d")
--- gui/utils/logger.py
+++
@@ -1,6 +0,0 @@
-import logging, os
-def setup_logger(path=None):
-    path = path or os.path.join(os.getcwd(), "attendance.log")
-    logging.basicConfig(filename=path, level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
-    return logging.getLogger("attendance_app")
